14402c28de2f8052babe1b460d061819f9786e08
modestmark <reffstar@gmail.com>
2019-02-10 22:47:17 -0700

    rotate bird sprite with last horizontal direction input

--- Birds.py
+++ Birds.py
@@ -7,7 +7,10 @@ class Bird(pygame.sprite.Sprite) :
 
     def __init__(self) :
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("bird_small.png")
+        self.imageFacing = dict()
+        self.imageFacing['left'] = pygame.image.load("bird_small.png")
+        self.imageFacing['right'] = pygame.transform.flip(self.imageFacing['left'], 1, 0)
+        self.image = self.imageFacing['right']
         self.rect = self.image.get_rect()
         self.speedx = 0
         self.speedz = 0
@@ -15,8 +18,10 @@ class Bird(pygame.sprite.Sprite) :
     def control(self, x, z) :
         if (self.speedx < 7) and (x > 0) : 
             self.speedx += x
+            self.image = self.imageFacing['right']
         elif (self.speedx > -7) and (x < 0) : 
             self.speedx += x
+            self.image = self.imageFacing['left']
         if (self.speedz < 7) and (z > 0) : 
             self.speedz += z
         elif (self.speedz > -7) and (z < 0) : 
