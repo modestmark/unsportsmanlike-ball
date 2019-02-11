6fdef479dbbc6ce6ce58cdf18e551bf8f1e63738
modestmark <reffstar@gmail.com>
2019-02-10 20:38:22 -0700

    add bird control and update methods

--- Birds.py
+++ Birds.py
@@ -4,8 +4,17 @@
 import pygame
 
 class Bird(pygame.sprite.Sprite) :
+
     def __init__(self) :
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("bird_small.png")
         self.rect = self.image.get_rect()
+
+    def control(self, x, z) :
+        self.movex += x
+        self.movez += z
+
+    def update(self) :
+        self.rect.x = self.rect.x + self.movex
+        self.rect.z = self.rect.z + self.movez
 		
